958cac5b56346eaa1c5fb4cbfdffd0519080ba3d
Svirid Arthur <134645532+BAToCAT@users.noreply.github.com>
2023-11-15 22:54:26 +0000

    modifing auth_page and reg_page

--- pages/auth_page.py
+++ pages/auth_page.py
@@ -1,38 +1,51 @@
 from pages.main_page import MainPage
 from locators import AuthLocators
+
+
 class AuthPage(MainPage):
 
-    def login_user(self, query: str, value: str) -> None:
+    def login_input_data(self, query):
         login = self.driver.find_element(*AuthLocators.user_name)
         login.send_keys(query)
+
+    def password_input_data(self, value):
         password = self.driver.find_element(*AuthLocators.user_pwd)
         password.send_keys(value)
+
+    def login_button_click(self):
         login_btn = self.driver.find_element(*AuthLocators.log_btn)
         login_btn.click()
 
-    def login_user_from_Belarus(self, query: str, value: str) -> None:
-        login = self.driver.find_element(*AuthLocators.user_name)
-        login.send_keys(query)
-        password = self.driver.find_element(*AuthLocators.user_pwd)
-        password.send_keys(value)
-
-
-    def help_menu(self) -> None:
+    def help_button_click(self):
         help_button = self.driver.find_element(*AuthLocators.help_btn)
         help_button.click()
 
-    def empty_tel(self) -> None:
+    def forgot_password_button_click(self):
         button = self.driver.find_element(*AuthLocators.fgt_btn)
         button.click()
+
+    def forward_button_click(self):
         forward = self.driver.find_element(*AuthLocators.forgot_forward)
         forward.click()
+
+    def login_user(self, query: str, value: str) -> None:
+        self.login_input_data(query)
+        self.password_input_data(value)
+        self.login_button_click()
+
+    def login_user_from_Belarus(self, query: str, value: str) -> None:
+        self.login_input_data(query)
+        self.password_input_data(value)
+
+    def help_menu(self) -> None:
+        self.help_button_click()
+
+    def empty_tel(self) -> None:
+        self.forgot_password_button_click()
+        self.forward_button_click()
 
     def wrong_tel(self, value) -> None:
-        button = self.driver.find_element(*AuthLocators.fgt_btn)
-        button.click()
-        user = self.driver.find_element(*AuthLocators.user_name)
-        user.send_keys(value)
-        forward = self.driver.find_element(*AuthLocators.forgot_forward)
-        forward.click()
-
+        self.forgot_password_button_click()
+        self.login_input_data(value)
+        self.forward_button_click()
 
--- pages/reg_page.py
+++ pages/reg_page.py
@@ -2,31 +2,54 @@ from pages.main_page import MainPage
 from locators import RegLocators
 from selenium.webdriver.common.keys import Keys
 
+
 class RegPage(MainPage):
 
-    def registr_user(self, nameu: str,
-                    last: str,
-                    region_name: str,
-                    reg_mail: str,
-                    reg_pwd: str,
-                    reg_pwd_rep: str ) -> None:
-
+    def registration_click_button(self):
         reg_btn = self.driver.find_element(*RegLocators.reg_btn)
         reg_btn.click()
+
+    def name_input_data(self, nameu):
         name = self.driver.find_element(*RegLocators.first_name)
         name.send_keys(nameu)
+
+    def last_user_name_input_data(self, last):
         last_user_name = self.driver.find_element(*RegLocators.last_name)
         last_user_name.send_keys(last)
+
+    def user_mail_input(self, reg_mail):
         user_mail = self.driver.find_element(*RegLocators.regi_mail)
         user_mail.send_keys(reg_mail)
+
+    def password_input_data(self, reg_pwd):
         user_passw = self.driver.find_element(*RegLocators.regi_pwd)
         user_passw.send_keys(reg_pwd)
+
+    def password_confirm_input_data(self, reg_pwd_rep):
         user_confpwd = self.driver.find_element(*RegLocators.regi_repassw)
         user_confpwd.send_keys(reg_pwd_rep)
+
+    def select_region(self, region_name):
         choice_reg = self.driver.find_element(*RegLocators.region)
         choice_reg.click()
         choice_reg.send_keys(Keys.BACKSPACE * 8)
         choice_reg.send_keys(region_name)
+
+    def submit_button_click(self):
         submit = self.driver.find_element(*RegLocators.subm_btn)
         submit.click()
 
+    def registr_user(self, nameu: str,
+                     last: str,
+                     region_name: str,
+                     reg_mail: str,
+                     reg_pwd: str,
+                     reg_pwd_rep: str) -> None:
+        self.registration_click_button()
+        self.name_input_data(nameu)
+        self.last_user_name_input_data(last)
+        self.user_mail_input(reg_mail)
+        self.password_input_data(reg_pwd)
+        self.password_confirm_input_data(reg_pwd_rep)
+        self.select_region(region_name)
+        self.submit_button_click()
