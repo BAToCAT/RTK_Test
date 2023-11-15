ff4b1f0e061b9f9f4995b4245edb7a1da9a208f1
Svirid Arthur <134645532+BAToCAT@users.noreply.github.com>
2023-11-15 18:45:20 +0300

    Add files via upload

---
+++ tests/test_login.py
@@ -0,0 +1,109 @@
+from pages.auth_page import AuthPage
+from settings import BASE_URL, DIFFERENT_URL, tel, bel_tel, pwd, inv_mail, oversize_login, recovery
+from time import sleep
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait as WDW
+from selenium.webdriver.common.by import By
+import requests
+
+
+def test_login(driver):
+    page = AuthPage(driver)
+    page.login_user(tel, pwd)
+    WDW(driver, timeout=3).until(EC.presence_of_element_located((By.XPATH, "//*[@id='app']/main/div/div[2]/div[1]/div[2]/div[1]/div/span[2]/span")))
+
+def test_user_from_Belarus(driver):
+    page = AuthPage(driver)
+    page.login_user_from_Belarus(bel_tel, pwd)
+    assert EC.invisibility_of_element_located(
+        (By.XPATH, '//*[@class="rt-input-container__meta rt-input-container__meta--error"]'))
+    WDW(driver, timeout=3).until(EC.invisibility_of_element_located(
+        (By.XPATH, '//*[@class="rt-input-container__meta rt-input-container__meta--error"]')))
+
+def test_invalid_mail(driver):
+    page = AuthPage(driver)
+    page.login_user(inv_mail, pwd)
+    # driver.save_screenshot("reg1.png")
+    wrong_login = driver.find_element(By.ID, "form-error-message").text
+    assert "Неверный логин или пароль" in wrong_login
+
+def test_remember_login(driver):
+    page = AuthPage(driver)
+    page.login_user(tel, pwd)
+    driver.get(DIFFERENT_URL)
+    driver.implicitly_wait(3)
+    driver.get(BASE_URL)
+    user_page = driver.find_element(By.XPATH, "//*[@id='app']/main/div/div[2]/div[1]/div[2]/div[1]/div/span[2]/span").text
+    assert tel in user_page
+    sleep(1)
+
+def test_logout(driver):
+    page = AuthPage(driver)
+    remember_btn = driver.find_element(By.XPATH, "//*[@class='rt-checkbox__label']")
+    remember_btn.click()
+    page.login_user(tel, pwd)
+    logout_btn = driver.find_element(By.ID, "logout-btn")
+    logout_btn.click()
+    atuth_main_page = driver.find_element(By.XPATH, "//*[text()='Авторизация']").text
+    assert 'Авторизация' in atuth_main_page
+
+def test_name_empty_field(driver):
+    page = AuthPage(driver)
+    page.login_user('','')
+    driver.save_screenshot("login_name_empty.png")
+    allert_empty_fields = driver.find_element(By.XPATH, "//*[text()='Введите номер телефона']").text
+    assert "Введите номер телефона" in allert_empty_fields
+
+def test_help(driver):
+    page = AuthPage(driver)
+    page.help_menu()
+    WDW(driver, timeout=3).until(EC.visibility_of_element_located((By.XPATH, '//*[@class="faq-modal__content"]')))
+
+def test_empty_telephone_num(driver):
+    page = AuthPage(driver)
+    page.empty_tel()
+    message = driver.find_element(By.XPATH, "//*[@id='page-right']/div/div[1]/div/form/div[1]/div[2]/span").text
+    assert "Введите номер телефона" in message
+
+def test_wrong_telephone_num(driver):
+    page = AuthPage(driver)
+    page.wrong_tel("1319")
+    message = driver.find_element(By.XPATH, '//*[@id="page-right"]/div/div[1]/div/form/div[1]/div[2]/span').text
+    assert "Неверный формат телефона" in message
+
+def test_captcha(driver):
+    page = AuthPage(driver)
+    page.empty_tel()
+    WDW(driver, timeout=3).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '*[alt="Captcha"]')))
+
+def test_forgot_password(driver):
+    # тест провалиться из-за капчи
+    # прописан вооброжаемый ключ recovery для поля ввода присланого на почту пароля
+    #  как составить ожидание прихода пароля с опросом и в случае если его нет нажать кнопку отправить повторно?
+
+    page = AuthPage(driver)
+    page.wrong_tel(tel)
+    mail_choice_btn = page.driver.find_element(By.XPATH, '//*[@class="rt-radio__label"]')
+    mail_choice_btn.click()
+    WDW(driver, timeout=10).until(EC.visibility_of_element_located((By.XPATH, '//*[class="card-container__title"]')))
+    field_num_recovery = driver.find_element(By.ID, 'id="rt-code-0"')
+    field_num_recovery.send_keys(recovery)
+
+def test_max_char(driver):
+    page = AuthPage(driver)
+    page.login_user(oversize_login, pwd)
+
+    # пробовал:
+    # url = driver.current_url
+    # response = requests.get(url)
+    # print(response.status_code)
+
+    # пробовал:
+    # driver.switch_to_window(driver.window_handles[1])
+    # url = driver.current_url
+    # response = requests.get(url)
+    # print(response.status_code)
+
+    sleep(3)
+    # driver.save_screenshot("reg1.png")
+    # как прописать редирект и отследить статус код?
---
+++ tests/test_registration.py
@@ -0,0 +1,78 @@
+
+from pages.reg_page import RegPage
+from settings import reg_first_name, reg_last_name, reg_mail, reg_pwd, reg_pwd_rep, reg_region, test_pwd, invalid_mail, test_pwd_chars_oly, different_pwd, Chinese_char, Latin, Spec_char
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait as WDW
+from selenium.webdriver.common.by import By
+
+
+
+def test_allready_registred(driver):
+    # переход на страницу и имитация регистрации с заполнением всех обязательных полей
+    page = RegPage(driver)
+    page.registr_user(reg_first_name, reg_last_name, reg_region, reg_mail, reg_pwd, reg_pwd_rep )
+    allready_allert = driver.find_element(By.XPATH, "//*[text()='Учётная запись уже существует']").text
+    assert "Учётная запись уже существует" in allready_allert
+    WDW(driver, timeout=3).until(EC.presence_of_element_located((By.CSS_SELECTOR, "*[name='firstName']")))
+
+def test_epty_name(driver):
+    page = RegPage(driver)
+    page.registr_user("", reg_last_name, reg_region, reg_mail, reg_pwd, reg_pwd_rep)
+    error = driver.find_element(By.XPATH, '//*[@class="rt-input-container__meta rt-input-container__meta--error"]').text
+    assert "Необходимо заполнить поле кириллицей. От 2 до 30 символов." in error
+    WDW(driver, timeout=3).until(EC.presence_of_element_located((By.CSS_SELECTOR, "*[name='firstName']")))
+
+def test_wrong_mail(driver):
+    page = RegPage(driver)
+    page.registr_user(reg_first_name, reg_last_name, reg_region, invalid_mail, reg_pwd, reg_pwd_rep)
+    WDW(driver, timeout=3).until(EC.presence_of_element_located((By.XPATH,'//*[text()="Введите телефон в формате +7ХХХХХХХХХХ или +375XXXXXXXXX, или email в формате example@email.ru"]')))
+    error = driver.find_element(By.XPATH,'//*[text()="Введите телефон в формате +7ХХХХХХХХХХ или +375XXXXXXXXX, или email в формате example@email.ru"]').text
+    assert 'Необходимо заполнить поле кириллицей. От 2 до 30 символов.' != (
+        'Введите телефон в формате +7ХХХХХХХХХХ или +375XXXXXXXXX, или email в формате example@email.ru') in error
+
+def test_Chinese_char(driver):
+    page = RegPage(driver)
+    page.registr_user(Chinese_char, reg_last_name, reg_region, reg_mail, reg_pwd, reg_pwd_rep)
+    error = driver.find_element(By.XPATH, '//*[@class="rt-input-container__meta rt-input-container__meta--error"]').text
+    assert "Необходимо заполнить поле кириллицей. От 2 до 30 символов." in error
+    WDW(driver, timeout=3).until(EC.presence_of_element_located((By.CSS_SELECTOR, "*[name='firstName']")))
+
+def test_Latin_char(driver):
+    page = RegPage(driver)
+    page.registr_user(reg_first_name, Latin, reg_region, reg_mail, reg_pwd, reg_pwd_rep)
+    error = driver.find_element(By.XPATH, '//*[@class="rt-input-container__meta rt-input-container__meta--error"]').text
+    assert "Необходимо заполнить поле кириллицей. От 2 до 30 символов." in error
+    WDW(driver, timeout=3).until(EC.presence_of_element_located((By.CSS_SELECTOR, "*[name='firstName']")))
+
+def test_Spec_char(driver):
+    page = RegPage(driver)
+    page.registr_user(reg_first_name, Latin, reg_region, Spec_char, reg_pwd, reg_pwd_rep)
+    WDW(driver, timeout=3).until(EC.presence_of_element_located((By.XPATH, '//*[text()="Введите телефон в формате +7ХХХХХХХХХХ или +375XXXXXXXXX, или email в формате example@email.ru"]')))
+    error = driver.find_element(By.XPATH, '//*[text()="Введите телефон в формате +7ХХХХХХХХХХ или +375XXXXXXXXX, или email в формате example@email.ru"]').text
+    assert 'Необходимо заполнить поле кириллицей. От 2 до 30 символов.' != ('Введите телефон в формате +7ХХХХХХХХХХ или +375XXXXXXXXX, или email в формате example@email.ru') in error
+
+def test_password_lover_case(driver):
+    page = RegPage(driver)
+    page.registr_user(reg_first_name, reg_last_name, reg_region, reg_mail, test_pwd, reg_pwd_rep)
+    error = driver.find_element(By.XPATH, '//*[text()="Пароль должен содержать хотя бы одну заглавную букву"]').text
+    assert "Пароль должен содержать хотя бы одну заглавную букву" in error
+    WDW(driver, timeout=3).until(EC.presence_of_element_located((By.CSS_SELECTOR, "*[name='firstName']")))
+
+def test_password_latin_chars_only(driver):
+    page = RegPage(driver)
+    page.registr_user(reg_first_name, reg_last_name, reg_region, reg_mail, test_pwd_chars_oly, reg_pwd_rep)
+    error = driver.find_element(By.XPATH, '//*[text()="Пароль должен содержать хотя бы 1 спецсимвол или хотя бы одну цифру"]').text
+    assert "Пароль должен содержать хотя бы 1 спецсимвол или хотя бы одну цифру" in error
+    WDW(driver, timeout=3).until(EC.presence_of_element_located((By.CSS_SELECTOR, "*[name='firstName']")))
+
+def test_different_paswords(driver):
+    page = RegPage(driver)
+    page.registr_user(reg_first_name, reg_last_name, reg_region, reg_mail, reg_pwd, different_pwd)
+    error = driver.find_element(By.XPATH,'//*[text()="Пароли не совпадают"]').text
+    assert "Пароли не совпадают" in error
+    WDW(driver, timeout=3).until(EC.presence_of_element_located((By.CSS_SELECTOR, "*[name='firstName']")))
+
+
+
+
+
